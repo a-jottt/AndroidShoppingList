b10f8c7041562109cf891b5f7c695612920c79e7
a-jottt <j.jasnowska@gmail.com>
2016-06-28 17:43:47 +0200

    Add Current and Archived fragments

---
+++ app/src/main/java/com/example/androidshoppinglist/views/fragments/ArchivedFragment.java
@@ -0,0 +1,23 @@
+package com.example.androidshoppinglist.views.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.example.androidshoppinglist.R;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Created by joanna on 28.06.16.
+ */
+@EFragment(R.layout.fragment_archived)
+public class ArchivedFragment extends Fragment {
+
+    public ArchivedFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
