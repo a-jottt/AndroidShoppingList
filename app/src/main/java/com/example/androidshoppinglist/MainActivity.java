7f5b2536d5338411e65398731f4ca90d6feaef4e
Joanna Jasnowska <j.jasnowska@gmail.com>
2016-06-28 23:33:18 +0200

    Add navigation drawer

--- app/src/main/java/com/example/androidshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/androidshoppinglist/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.androidshoppinglist;
 
-import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
@@ -12,28 +17,76 @@ import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
-public class MainActivity extends AppCompatActivity {
+@EActivity(R.layout.activity_drawer)
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @ViewById(R.id.toolbar) Toolbar toolbar;
     @ViewById(R.id.fab) FloatingActionButton fab;
-
-    private Context mContext;
-
+    @ViewById(R.id.drawer_layout) DrawerLayout drawer;
+    @ViewById(R.id.nav_view) NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext = this;
     }
 
     @AfterViews
     public void prepare() {
         setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Click(R.id.fab)
     public void onAddListClick() {
-        Toast.makeText(mContext, "Add action", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Add action", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_current) {
+
+        } else if (id == R.id.nav_archived) {
+
+        } else if (id == R.id.nav_settings) {
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
