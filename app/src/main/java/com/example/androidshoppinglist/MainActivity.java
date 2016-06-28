293fddfa8cf18db29a22b9ecfbf8487e9d6923e4
Joanna Jasnowska <j.jasnowska@gmail.com>
2016-06-28 22:51:56 +0200

    Change main activity layout to collapsing toolbar layout

--- app/src/main/java/com/example/androidshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/androidshoppinglist/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.androidshoppinglist;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.content.Context;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-
-import com.example.androidshoppinglist.views.fragments.ArchivedFragment;
-import com.example.androidshoppinglist.views.fragments.CurrentFragment;
-import com.example.androidshoppinglist.views.adapters.ViewPagerAdapter;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
@@ -18,36 +16,24 @@ import org.androidannotations.annotations.ViewById;
 public class MainActivity extends AppCompatActivity {
 
     @ViewById(R.id.toolbar) Toolbar toolbar;
-    @ViewById(R.id.viewpager) ViewPager viewPager;
-    @ViewById(R.id.tabs) TabLayout tabLayout;
+    @ViewById(R.id.fab) FloatingActionButton fab;
 
-    private int[] tabIcons = {
-            R.drawable.basket,
-            R.drawable.av_timer
-    };
+    private Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
     }
 
     @AfterViews
-    public void prepareView() {
+    public void prepare() {
         setSupportActionBar(toolbar);
-        setupViewPager(viewPager);
-        tabLayout.setupWithViewPager(viewPager);
-        setupTabIcons();
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new CurrentFragment(), getString(R.string.current_tab_title));
-        adapter.addFragment(new ArchivedFragment(), getString(R.string.archived_tab_title));
-        viewPager.setAdapter(adapter);
-    }
-
-    private void setupTabIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+    @Click(R.id.fab)
+    public void onAddListClick() {
+        Toast.makeText(mContext, "Add action", Toast.LENGTH_LONG).show();
     }
 }
