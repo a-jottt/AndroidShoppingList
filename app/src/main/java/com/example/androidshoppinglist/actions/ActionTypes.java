526d6f76f1fb3e466232444d6ed59fdb28d1dc03
a-jottt <j.jasnowska@gmail.com>
2016-06-29 15:18:49 +0200

    Add ActionTypes and DataKeys classes to define actions and keys

---
+++ app/src/main/java/com/example/androidshoppinglist/actions/ActionTypes.java
@@ -0,0 +1,9 @@
+package com.example.androidshoppinglist.actions;
+
+/**
+ * Created by joanna on 29.06.16.
+ */
+public enum ActionTypes {
+
+    ADD_NEW_SHOPPING_LIST
+}
---
+++ app/src/main/java/com/example/androidshoppinglist/actions/DataKeys.java
@@ -0,0 +1,9 @@
+package com.example.androidshoppinglist.actions;
+
+/**
+ * Created by joanna on 29.06.16.
+ */
+public enum DataKeys {
+
+    TITLE
+}
