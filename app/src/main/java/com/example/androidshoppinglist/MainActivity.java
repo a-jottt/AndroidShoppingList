927a52eff9712f937c7f42a43bc2257650a6fe32
a-jottt <j.jasnowska@gmail.com>
2016-06-29 14:54:18 +0200

    Add recyclerView + set adapter in MainActivity with test content

--- app/src/main/java/com/example/androidshoppinglist/MainActivity.java
+++ app/src/main/java/com/example/androidshoppinglist/MainActivity.java
@@ -7,15 +7,24 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.androidshoppinglist.models.ShoppingListItem;
+import com.example.androidshoppinglist.views.adapters.ShoppingListAdapter;
+
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @EActivity(R.layout.activity_drawer)
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -24,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @ViewById(R.id.fab) FloatingActionButton fab;
     @ViewById(R.id.drawer_layout) DrawerLayout drawer;
     @ViewById(R.id.nav_view) NavigationView navigationView;
+    @ViewById(R.id.recyclerView) RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(this);
+
+        List<ShoppingListItem> items = new ArrayList<>();
+        items.add(new ShoppingListItem("my first", new Date()));
+        items.add(new ShoppingListItem("my second", new Date()));
+        items.add(new ShoppingListItem("my last", new Date()));
+
+        ShoppingListAdapter adapter = new ShoppingListAdapter(items, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
     @Click(R.id.fab)
