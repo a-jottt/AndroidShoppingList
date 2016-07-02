00b812d496129eff951d26c25e9641b22ab5c599
Joanna Jasnowska <j.jasnowska@gmail.com>
2016-07-02 23:40:21 +0200

    Setup realm

--- app/src/main/java/com/example/androidshoppinglist/app/BaseApplication.java
+++ app/src/main/java/com/example/androidshoppinglist/app/BaseApplication.java
@@ -2,12 +2,17 @@ package com.example.androidshoppinglist.app;
 
 import android.app.Application;
 
+import com.example.androidshoppinglist.BuildConfig;
 import com.example.androidshoppinglist.app.modules.ApplicationModule;
 import com.example.androidshoppinglist.views.MainActivity;
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
 
 import javax.inject.Singleton;
 
 import dagger.Component;
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
 
 /**
  * Created by joanna on 29.06.16.
@@ -30,6 +35,17 @@ public class BaseApplication extends Application {
                 .applicationModule(new ApplicationModule(this))
                 .build();
         component().inject(this);
+
+        RealmConfiguration config = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(config);
+
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                            .build());
+        }
     }
 
     public ApplicationComponent component() {
