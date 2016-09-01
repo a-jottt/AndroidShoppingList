3854abaa8becc89a44a5be484dcc71078cf2418f
Joanna Jasnowska <j.jasnowska@gmail.com>
2016-09-01 20:08:51 +0200

    ShoppingListStore refactoring: remove switch

--- app/src/main/java/com/example/androidshoppinglist/stores/ShoppingListStore.java
+++ app/src/main/java/com/example/androidshoppinglist/stores/ShoppingListStore.java
@@ -1,15 +1,11 @@
 package com.example.androidshoppinglist.stores;
 
 import com.example.androidshoppinglist.actions.ActionTypes;
-import com.example.androidshoppinglist.actions.DataBundle;
 import com.example.androidshoppinglist.actions.DataKeys;
 import com.example.androidshoppinglist.actions.ShoppingListAction;
 import com.example.androidshoppinglist.data.ActivityEvent;
 import com.example.androidshoppinglist.data.ArchiveOrDeleteListEvent;
-import com.example.androidshoppinglist.data.DeleteProductEvent;
 import com.example.androidshoppinglist.data.GetListActionType;
-import com.example.androidshoppinglist.data.ProductBoughtEvent;
-import com.example.androidshoppinglist.models.Product;
 import com.example.androidshoppinglist.models.ShoppingListItem;
 
 import org.greenrobot.eventbus.EventBus;
@@ -42,47 +38,41 @@ public class ShoppingListStore {
     }
 
     @Subscribe
-    public final void onShoppingListAction(ShoppingListAction action) {
-        DataBundle<DataKeys> data = action.getData();
-        ActionTypes actionType = action.getType();
-        switch (actionType) {
-            case ADD_NEW_SHOPPING_LIST:
-                String title = (String) data.get(DataKeys.SHOPPING_LIST_TITLE, -1);
-                addNewShoppingList(title);
-                break;
-            case GET_SHOPPING_LISTS_FROM_DATABASE:
-                GetListActionType listType = (GetListActionType) data.get(DataKeys.GET_LIST_ACTION_TYPE, -1);
-                getShoppingListsFromDb(ActionTypes.GET_SHOPPING_LISTS_FROM_DATABASE, listType);
-                break;
-            case ARCHIVE_LIST:
-                long archivedListCreatedAtTime = (long) data.get(DataKeys.LIST_CREATED_AT_TIME, -1);
-                archiveList(archivedListCreatedAtTime);
-                break;
-            case DELETE_LIST:
-                long deletedListCreatedAtTime = (long) data.get(DataKeys.LIST_CREATED_AT_TIME, -1);
-                deleteList(deletedListCreatedAtTime);
-                break;
+    public void addNewShoppingList(ShoppingListAction action) {
+        if (action.getType().equals(ActionTypes.ADD_NEW_SHOPPING_LIST)) {
+            String title = (String) action.getData().get(DataKeys.SHOPPING_LIST_TITLE, -1);
+            ShoppingListItem shoppingListItem = new ShoppingListItem(title, new Date());
+            eventBus.post(shoppingListItem);
         }
     }
 
-    private void getShoppingListsFromDb(ActionTypes actionType, GetListActionType listType) {
-        ActivityEvent activityEvent = new ActivityEvent(actionType);
-        activityEvent.setGetListActionType(listType);
-        eventBus.post(activityEvent);
+    @Subscribe
+    public void getShoppingListsFromDb(ShoppingListAction action) {
+        ActionTypes actionType = ActionTypes.GET_SHOPPING_LISTS_FROM_DATABASE;
+        if (action.getType().equals(actionType)) {
+            GetListActionType listType = (GetListActionType) action.getData().get(DataKeys.GET_LIST_ACTION_TYPE, -1);
+            ActivityEvent activityEvent = new ActivityEvent(actionType);
+            activityEvent.setGetListActionType(listType);
+            eventBus.post(activityEvent);
+        }
+
     }
 
-    private void addNewShoppingList(String title) {
-        ShoppingListItem shoppingListItem = new ShoppingListItem(title, new Date());
-        eventBus.post(shoppingListItem);
+    @Subscribe
+    public void archiveList(ShoppingListAction action) {
+        if (action.getType().equals(ActionTypes.ARCHIVE_LIST)) {
+            long archivedListCreatedAtTime = (long) action.getData().get(DataKeys.LIST_CREATED_AT_TIME, -1);
+            eventBus.post(new ArchiveOrDeleteListEvent(archivedListCreatedAtTime,
+                    ArchiveOrDeleteListEvent.EventStatus.ARCHIVE));
+        }
     }
 
-    private void archiveList(long archivedListCreatedAtTime) {
-        eventBus.post(new ArchiveOrDeleteListEvent(archivedListCreatedAtTime,
-                ArchiveOrDeleteListEvent.EventStatus.ARCHIVE));
-    }
-
-    private void deleteList(long deletedListCreatedAtTime) {
-        eventBus.post(new ArchiveOrDeleteListEvent(deletedListCreatedAtTime,
-                ArchiveOrDeleteListEvent.EventStatus.DELETE));
+    @Subscribe
+    public void deleteList(ShoppingListAction action) {
+        if (action.getType().equals(ActionTypes.DELETE_LIST)) {
+            long deletedListCreatedAtTime = (long) action.getData().get(DataKeys.LIST_CREATED_AT_TIME, -1);
+            eventBus.post(new ArchiveOrDeleteListEvent(deletedListCreatedAtTime,
+                    ArchiveOrDeleteListEvent.EventStatus.DELETE));
+        }
     }
 }
