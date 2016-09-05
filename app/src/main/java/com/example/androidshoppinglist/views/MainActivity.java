1efed33409f6dfe7cf872e97606803bd7412e467
Joanna Jasnowska <j.jasnowska@gmail.com>
2016-09-05 22:35:33 +0200

    Remove action settings and setting option from drawer

--- app/src/main/java/com/example/androidshoppinglist/views/MainActivity.java
+++ app/src/main/java/com/example/androidshoppinglist/views/MainActivity.java
@@ -13,11 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.androidshoppinglist.R;
@@ -124,24 +122,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.drawer, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -152,8 +132,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         } else if (id == R.id.nav_archived) {
             actionCreator.createGetShoppingListsFromDbAction(GetListActionType.ARCHIVED);
-
-        } else if (id == R.id.nav_settings) {
 
         }
 
